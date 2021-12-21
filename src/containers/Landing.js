458592c06a2aa05593cfd006d7eeebf6c08b1964
Hunter <fleakhunter@gmail.com>
2021-12-21 12:46:47 -0500

    Set up react-router; create-react-app clean-up.

---
+++ src/containers/Landing.js
@@ -0,0 +1,11 @@
+const Landing = () => {
+    return (
+        <>
+        <h1>CHROMEO</h1>
+        <h3>A music visualizer using the Spotify API.</h3>
+        <Link>Sign-up</Link>
+        </>
+    )
+}
+
+export default Landing;
