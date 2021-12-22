39e84bd68c0d13bf97cff7176f73b4f5abef46a3
Hunter <fleakhunter@gmail.com>
2021-12-22 11:46:26 -0500

    Add login form.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,34 @@
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 const Login = () => {
     return (
-        <h1>Hello World</h1>
+        <>
+        <h1>Log-in to use Chromeo's premier music discoverability features.</h1>
+
+        <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+                Login
+            </Button>
+        </Form>
+        </>
     )
 }
 
