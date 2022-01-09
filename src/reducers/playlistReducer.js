f315dec02ce914f2224aae3c6a35f9349bcb9649
Hunter <fleakhunter@gmail.com>
2022-01-09 14:44:10 -0500

    Fix playlist state.

--- src/reducers/playlistReducer.js
+++ src/reducers/playlistReducer.js
@@ -1,9 +1,14 @@
-const playlistReducer = (state = {playlistID: '', tracks: []}, action) => {
+const playlistReducer = (state = 
+    {playlistID: null, playlistName: null, tracks: [], message: ''},
+    action) => {
     switch (action.type) {
         case 'FETCH_TRACKS':
-            return action.payload
+            return {...state, tracks: action.payload};
+        case 'SPOTIFY_ERROR':
+            console.log("error in reducer");
+            return {...state, message: action.payload};
         default:
-            return state;
+            return {...state};
     }
 
 
