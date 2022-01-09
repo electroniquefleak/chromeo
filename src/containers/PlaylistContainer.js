623df3a41e647e8f7ba0ca8bce6a2930b4796e78
Hunter <fleakhunter@gmail.com>
2022-01-09 14:45:33 -0500

    Add theme, track cards, and redux state/dispatch.

--- src/containers/PlaylistContainer.js
+++ src/containers/PlaylistContainer.js
@@ -1,8 +1,42 @@
-const PlaylistContainer = () => {
+import { Stack, Divider, Paper } from "@mui/material";
+import { connect } from "react-redux";
+import TrackCard from "../components/TrackCard";
+
+import { styled } from '@mui/material/styles';
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+const PlaylistContainer = ({playlist}) => {
+    const {playlistName: name, tracks} = playlist;
     return (
-        <div>
-            This will have the nested components of Track 
-        </div>
+        <>
+            {name && <h1>{name}</h1>}
+            <Stack 
+                divider={<Divider orientation="horizontal" flexItem />}
+                spacing={2}
+            >
+                {tracks.map(track => {
+                    return (
+                        <Item key={track.id}>
+                            <TrackCard
+                                title={track.title}
+                                artist={track.artist}
+                                length={track.length}
+                                art={track.art}
+                            />
+                        </Item>
+                    )
+                })}
+            </Stack>
+        </>
     )
 }
-export default PlaylistContainer;
+const mapStateToProps = state => {
+    return {playlist: state.playlist}
+}
+export default connect(mapStateToProps)(PlaylistContainer);
