302fdd0cf515fffc043a90c992993136874b4a03
Hunter <fleakhunter@gmail.com>
2022-01-09 14:49:47 -0500

    Import LoggedIn component and add route path.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Dash from './containers/Dash';
 import Landing from './containers/Landing';
 import Signup from './components/Signup';
 import Login from './components/Login';
+import LoggedIn from './components/LoggedIn'
 import { Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -10,6 +11,7 @@ function App() {
   return (
   <Routes>
     <Route path="/" element={<Landing />} />
+    <Route path='/token/:id' element={<LoggedIn />} />
     <Route path="/signup" element={<Signup />} />
     <Route path="/login" element={<Login />} />
     <Route path="/dashboard" element={<Dash />} />
