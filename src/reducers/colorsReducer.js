9d642232939d5cbeefc6a9027fbe81b9f4b191b0
Hunter <fleakhunter@gmail.com>
2022-01-10 22:35:35 -0500

    Genre swap on colors.

--- src/reducers/colorsReducer.js
+++ src/reducers/colorsReducer.js
@@ -2,62 +2,74 @@ const initialColors = [
     {
         id: 1,
         hex: '#FF0000',
-        isSelected: false
+        isSelected: false,
+        genre: 'classical'
     },
     {
         id: 2,
         hex: '#E67E22',
-        isSelected: false
+        isSelected: false,
+        genre: 'country'
     },
     {
         id: 3,
         hex: '#F4D03F',
-        isSelected: false
+        isSelected: false,
+        genre: 'rock'
     },
     {
         id: 4,
         hex: '#28B463',
-        isSelected: false
+        isSelected: false,
+        genre: 'pop'
     },
     {
         id: 5,
         hex: '#DDF0F3',
-        isSelected: false
+        isSelected: false,
+        genre: 'pop'
     },
     {
         id: 6,
         hex: '#D4E6F1',
-        isSelected: false
+        isSelected: false,
+        genre: 'pop'
     },
     {
         id: 7,
         hex: '#898DFC',
-        isSelected: false
+        isSelected: false,
+        genre: 'pop'
     },
     {
         id: 8,
         hex: '#9152FF',
-        isSelected: false
+        isSelected: false,
+        genre: 'pop'
     },
     {
         id: 9,
         hex: '#EAA4FF',
-        isSelected: false
+        isSelected: false,
+        genre: 'pop'
     },
     {
         id: 10,
         hex: '#E95793',
-        isSelected: false
+        isSelected: false,
+        genre: 'pop'
     },
     {
         id: 11,
         hex: '#C48787',
-        isSelected: false
+        isSelected: false,
+        genre: 'country'
     },
     {
         id: 12,
         hex: '#CF223C',
-        isSelected: false
+        isSelected: false,
+        genre: 'pop'
     },
 ];
 const colorsReducer = (state = initialColors, action) => {
