e46f6b2f62bcf77458fbccdd24b16ec66bbcac79
Hunter <fleakhunter@gmail.com>
2022-01-12 23:39:17 -0500

    Add Spotify api url  endpoint parts.

--- src/services/spotify_setup.js
+++ src/services/spotify_setup.js
@@ -1,3 +1,3 @@
 export const authEndpoint = 'https://accounts.spotify.com/authorize/?';
 export const clientId = '7cb4cab81cd5485c97c8c54a7711fc67';
-export const redirectUri = "http://localhost:3000/token/";
+export const redirectUri = "http://localhost:3001/token/";
