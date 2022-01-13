4e9bf3b353e9242c753da05cf302462e91519058
Hunter <fleakhunter@gmail.com>
2022-01-12 23:42:40 -0500

    Refactor reducers.

--- src/reducers/colorsReducer.js
+++ src/reducers/colorsReducer.js
@@ -3,73 +3,85 @@ const initialColors = [
         id: 1,
         hex: '#FF0000',
         isSelected: false,
-        genre: 'classical'
+        genre: 'classical',
+        message: 'Message goes here'
     },
     {
         id: 2,
         hex: '#E67E22',
         isSelected: false,
-        genre: 'country'
+        genre: 'country',
+        message: 'Message goes here'
     },
     {
         id: 3,
         hex: '#F4D03F',
         isSelected: false,
-        genre: 'rock'
+        genre: 'rock',
+        message: 'Message goes here'
     },
     {
         id: 4,
         hex: '#28B463',
         isSelected: false,
-        genre: 'pop'
+        genre: 'pop',
+        message: 'Message goes here'
     },
     {
         id: 5,
         hex: '#DDF0F3',
         isSelected: false,
-        genre: 'pop'
+        genre: 'hip-hop',
+        message: 'Message goes here'
     },
     {
         id: 6,
         hex: '#D4E6F1',
         isSelected: false,
-        genre: 'pop'
+        genre: 'pop',
+        message: 'Message goes here'
     },
     {
         id: 7,
         hex: '#898DFC',
         isSelected: false,
-        genre: 'pop'
+        genre: 'pop',
+        message: 'Message goes here'
     },
     {
         id: 8,
         hex: '#9152FF',
         isSelected: false,
-        genre: 'pop'
+        genre: 'pop',
+        message: 'Message goes here'
     },
     {
         id: 9,
         hex: '#EAA4FF',
         isSelected: false,
-        genre: 'pop'
+        genre: 'pop',
+        message: 'Message goes here'
     },
     {
         id: 10,
         hex: '#E95793',
         isSelected: false,
-        genre: 'pop'
+        genre: 'pop',
+        message: 'Message goes here'
     },
     {
         id: 11,
         hex: '#C48787',
         isSelected: false,
-        genre: 'country'
+        genre: 'country',
+        message: 'Message goes here'
     },
     {
         id: 12,
         hex: '#CF223C',
         isSelected: false,
-        genre: 'pop'
+        genre: 'pop',
+        message: 'Message goes here'
     },
 ];
 const colorsReducer = (state = initialColors, action) => {
@@ -82,6 +94,8 @@ const colorsReducer = (state = initialColors, action) => {
                     return color
                 }
             })
+        case 'RESET_COLORS':
+            return initialColors
         default:
             return [...state];
     }
--- src/reducers/playlistReducer.js
+++ src/reducers/playlistReducer.js
@@ -1,17 +1,18 @@
-const playlistReducer = (state = 
-    {playlistID: null, playlistName: null, tracks: [], message: ''},
-    action) => {
+const initialState = {playlistID: null, playlistName: null, tracks: [], error: ''};
+const playlistReducer = (state = initialState, action) => {
     switch (action.type) {
-        case 'FETCH_TRACKS':
+        case 'FETCH_TRACKS_CHROMEO':
+            return {...state, tracks: action.tracks, playlistID: action.playlistID, playlistName: action.name};
+        case 'FETCH_TRACKS_SPOTIFY':
             return {...state, tracks: action.payload};
+        case 'RESET_PLAYLIST':
+            return initialState;
         case 'SPOTIFY_ERROR':
             console.log("error in reducer");
-            return {...state, message: action.payload};
+            return {...state, error: action.payload};
         default:
             return {...state};
     }
-
-
 }
 
 export default playlistReducer;
